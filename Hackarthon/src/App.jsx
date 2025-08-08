611cc14cc6524e3eb0b8ed06842a54184931eadf
thdms1 <soeun0579@naver.com>
2025-08-09 01:10:03 +0900

    [add] tailwind

--- Hackarthon/src/App.jsx
+++ Hackarthon/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <h1 className="text-3xl">hello</h1>;
+  return (
+    <div>
+        <h1 className="text-3xl text-blue-500">hello</h1>
+    </div>
+  );
 };
 
 export default App;
