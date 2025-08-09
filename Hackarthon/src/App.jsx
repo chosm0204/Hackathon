b1a797f600f2afd6f5f757c057ea855720b6beea
chosm0204 <choseung1234@naver.com>
2025-08-09 13:43:02 +0900

    tailwind 수정 임시 저장

--- Hackarthon/src/App.jsx
+++ Hackarthon/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 const App = () => {
-  return <h1 className="text-3xl">hello</h1>;
+  return (
+    <div className="text-3xl text-blue-600 font-bold">Tailwind 잘 적용됨!</div>
+  );
 };
 
 export default App;
---
+++ Hackarthon/src/cho/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <h1>어디서 뭐하지?</h1>
+    </div>
+  );
+};
+
+export default Main;
