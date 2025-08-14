8b317d7cf90b7e229d4feea221ec1de3866e3be3
thdms1 <soeun0579@naver.com>
2025-08-14 21:40:22 +0900

    [add] LoadingPage.jsx [fix] App.jsx

--- Hackarthon/src/App.jsx
+++ Hackarthon/src/App.jsx
@@ -1,34 +1,33 @@
-// App.jsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Testpage from "./Kim/Testpage";
+import {Routes, Route } from "react-router-dom";
 import Temporarily from "./Kim/Temporarily";
 import "./App.css";
 import Header from "./yang/Header";
 import Footer from "./yang/Footer";
 import Login from "./yang/Login";
+import Fake from "./yang/Fake";
 import Signup from "./yang/Signup";
 import Main from "./cho/Main";
+import LoadingPage from "./Kim/LoadingPAge";
 
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        {/* 메인 페이지 */}
+        {/* Updated upstream 라우트 */}
         <Route path="/" element={<Main />} />
-
-        {/* 기존 페이지들 */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
-        {/* 추가 페이지들 */}
-        <Route path="/testpage" element={<Testpage />} />
+        {/* Stashed changes 라우트 */}
+        <Route path="/Loading" element={<LoadingPage />} />
         <Route path="/temporarily" element={<Temporarily />} />
       </Routes>
       <Footer />
     </>
   );
 };
+
 
 export default App;
